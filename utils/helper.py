a06fddd501ad99abaa2605aaaeb730804bb2b43f
Priya Aryav <priya.aryav@gmail.com>
2024-10-03 23:14:24 +0530

    adding scratch methods

--- utils/helper.py
+++ utils/helper.py
@@ -1,2 +1,25 @@
+import numpy as np
+
 def test():
     print("Hello, World!")
+
+def predict(feature, parameter, intercept):
+    prediction = 0
+    n = feature.shape[0]
+    for i in range(n):
+        prediction += feature[i] * parameter[i]
+    prediction += intercept
+    return prediction
+
+def predict_numpy(feature, parameter, intercept):
+    prediction = np.dot(feature, parameter) + intercept
+    return prediction
+
+def compute_cost(x_test, y_test, w, b):
+    n = x_test.shape[0]
+    cost = 0.0
+    for i in range(n):
+        f_wb_i = np.dot(x_test[i], y_test[i]) + b
+        cost = (f_wb_i - y_test[i]) ** 2
+    cost = cost / (2 * n)
+    return cost
