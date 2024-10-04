55a9ab4be490f8868e96bfb061defe782917e2a7
Priya Aryav <priya.aryav@gmail.com>
2024-10-04 23:31:49 +0530

    added more methods

--- utils/helper.py
+++ utils/helper.py
@@ -29,3 +29,19 @@ def compute_cost(x_test, y_test, w, b):
 
 # Gradien Descent calculation
 # Baiscally it adjusts the parameter and intercept to get the best results from model
+def compute_gradient(x_train, y_train, w, b):
+    m, n = x_train.shape
+    dj_dw = np.zeros((n,))
+    dj_db = 0.
+    for i in range(m):
+        err = (np.dot(x_train[i], w) + b) - y_train[i]
+        for j in range(n):
+            dj_dw[j] = dj_dw[j] + err * x_train[i, j]
+        dj_db += err
+    dj_dw = dj_dw / m
+    dj_db = dj_db / m
+    return dj_dw, dj_db
+
+# Method to train the model or in other words fitting the model
+
+def fit(x_train, y_train, w, b, compute_cost, compute, gradient):
